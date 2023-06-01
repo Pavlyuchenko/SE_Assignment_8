7168031b3d2af5a21b25a9c9010d36ebd7959450
Michal Pavlíček <michaelg.pavlicek@gmail.com>
2023-06-01 19:07:40 +0200

    Initial with sample test

---
+++ GameTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+public class GameTest {
+    @Test
+    public void testAddPlayer() {
+        assertEquals(true, true);
+    }
+}
